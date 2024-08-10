c8b58e7e294524af3d833b63d33ed4978b0fe9ff
Muhammed2024Salama <devmuhammedsalama@gmail.com>
2024-08-10 13:07:31 +0300

    Setting Up Book Controller

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -28,8 +28,8 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
+                ->prefix('api/V1')
+                ->group(base_path('routes/V1/api.php'));
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
